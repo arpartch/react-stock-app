35b2269700a43f7d0ee795ce8128f987333a7680
arpartch <athena.partch@gmail.com>
2022-07-01 13:41:35 -0700

    2 files modified

--- src/components/StockData.jsx
+++ src/components/StockData.jsx
@@ -2,11 +2,34 @@ import React from 'react';
 import * as R from 'ramda';
 import Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
-import AAPL from './StubData/APPL.js';
+import AAPL from '../StubData/AAPL.js';
 
-const deriveCandleStickData = R.identity;
+// Get data out of the obj
+// Use props to get the keys in the correct order
+// Convert date to ms time stamp
+const deriveCandleStickData = R.pipe(
+  R.prop('data'),
+  R.map(R.props(['date', 'open', 'low', 'high', 'close']))
+);
+
+// adj_close: 135.43
+// adj_high: null
+// adj_low: null
+// adj_open: null
+// adj_volume: null
+// close: 135.43
+// date: "2022-06-15T00:00:00+0000"
+// dividend: 0
+// exchange: "XNAS"
+// high: 137.34
+// low: 132.16
+// open: 134.29
+// split_factor: 1
+// symbol: "AAPL"
+// volume: 91352700
 
 const data = deriveCandleStickData(AAPL);
+console.log(data);
 
 export const StockData = () => {
   const options = {
@@ -16,11 +39,7 @@ export const StockData = () => {
     series: [
       {
         type: 'candlestick',
-        data: [
-          [1593610200000, 91.28, 91.84, 90.98, 91.03],
-          [1593696600000, 91.96, 92.62, 90.91, 91.03],
-          [1593696800000, 91.96, 92.62, 90.91, 91.03],
-        ],
+        data,
       },
     ],
   };
