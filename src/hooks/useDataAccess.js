833e973898b95126505dd599a1b0721e493c777a
arpartch <athena.partch@gmail.com>
2022-07-24 11:07:28 -0700

    1 file modified

--- src/hooks/useDataAccess.js
+++ src/hooks/useDataAccess.js
@@ -3,7 +3,24 @@ import { useReducer } from 'react';
 
 const handleBuyShares = (state, action) => {
   console.log('buyingShares');
-  return state;
+  // buyingPower 1000 => 0
+  // positions 0 => 10
+  // sharePrice 100 => 100
+  const sharePrice = R.prop('sharePrice', state);
+  const buyingPower = R.prop('buyingPower', state);
+  const positions = R.prop('positions', state);
+  const availableShares = R.divide(buyingPower, sharePrice);
+  const cost = R.multiply(availableShares, sharePrice);
+  // sharePrice: 100
+  // buyingPower: 1000
+  // availableShares: 10
+  // cost: 1000
+  const newBuyingPower = R.subtract(buyingPower, cost);
+  const newPositions = R.add(positions, availableShares);
+  return R.pipe(
+    R.assoc('buyingPower', newBuyingPower),
+    R.assoc('positions', newPositions),
+  )(state);
 };
 
 const isBuyShares = R.pipe(
@@ -18,7 +35,7 @@ const stateReducer = (state, action) => {
   ])(state, action);
 };
 
-const initialState = { buyingPower: 1000, positions: 5, sharePrice: 100 };
+const initialState = { buyingPower: 1000, positions: 0, sharePrice: 100 };
 
 export const useDataAccess = () => {
   const stateControl = useReducer(stateReducer, initialState);
